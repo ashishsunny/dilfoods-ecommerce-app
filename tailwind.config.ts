4cdb9bfe8e24db570729464156e3db9081bb9346
Ashish Sunny <ashishsunny.dev@gmail.com>
2023-11-27 21:38:17 +0530

    Basic structure set | Tailwind setup | +Dependecies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        gray: "#fafafa",
+        black: "#000",
+        whitesmoke: "#f1f1f1",
+        white: "#fff",
+        peru: "#d87d4a",
+      },
+      spacing: {},
+      fontFamily: {
+        sans: ['var(--font-manrope)'],
+      },
+      borderRadius: {
+        "3xs": "10px",
       },
     },
+    fontSize: {
+      base: "1rem",
+      "9xl": "1.75rem",
+      inherit: "inherit",
+    },
   },
-  plugins: [],
-}
-export default config
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [require("daisyui")]
+};
